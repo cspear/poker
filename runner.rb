e9ee30291326fc6848af3912566d1dd7b2595828
Charlie Spear <cspear@rallydev.com>
2013-12-15 23:17:49 -0700

    added --help.  put in instructions on what is being expected

--- runner.rb
+++ runner.rb
@@ -31,28 +31,38 @@ if ARGV.first == nil
   puts "Congratulations: #{winner} (hand rank of: #{last_hand_rank})"
 
 else
-  # :two, :three, :four, :five, :six, :seven, :eight, :nine, :ten, :jack, :queen, :king, :ace
-  argv_cards = []
-  raise "wrong number of cards, only enter 5" unless ARGV.count == 5
+  if ARGV.first == "--help"
+    puts "poker.rb help"
+    puts " "
+    puts "if you want to evaluate a custom hand you must supply 5 cards in a"
+    puts "format like this: <number><one-letter suit>.  for example: 5d 10h"
+    puts "a for ace, k for king, q for queen, j for jack"
+    puts " "
+    puts "ruby runner.rb 5h 6h 7h 8h 9h"
+    puts " "
+  else
+    argv_cards = []
+    raise "wrong number of cards, only enter 5" unless ARGV.count == 5
 
-  ARGV.each do |card|
-    suit = card.byteslice(-1)
-    if suit == "s" then suit = :spades   end
-    if suit == "c" then suit = :clubs    end
-    if suit == "h" then suit = :hearts   end
-    if suit == "d" then suit = :diamonds end
+    ARGV.each do |card|
+      suit = card.byteslice(-1)
+      if suit == "s" then suit = :spades   end
+      if suit == "c" then suit = :clubs    end
+      if suit == "h" then suit = :hearts   end
+      if suit == "d" then suit = :diamonds end
 
-    argv_number = card[0...-1]
-    if argv_number == "a" then argv_number = "14" end
-    if argv_number == "k" then argv_number = "13" end
-    if argv_number == "q" then argv_number = "12" end
-    if argv_number == "j" then argv_number = "11" end
+      argv_number = card[0...-1]
+      if argv_number == "a" then argv_number = "14" end
+      if argv_number == "k" then argv_number = "13" end
+      if argv_number == "q" then argv_number = "12" end
+      if argv_number == "j" then argv_number = "11" end
 
-    number = Card::NUMBERS[(argv_number.to_i - 2)]
-    argv_cards << Card.new(number, suit)
+      number = Card::NUMBERS[(argv_number.to_i - 2)]
+      argv_cards << Card.new(number, suit)
+    end
+      argv_hand = Hand.new("-user inputed hand-", argv_cards)
+      returned = argv_hand.rank_hand
   end
-    argv_hand = Hand.new("self selected", argv_cards)
-    returned = argv_hand.rank_hand
 end
 
 
